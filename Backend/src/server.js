c4c7c1923f9089cbefe8d50ed745e76d22a9afb6
PedrosoEnzo <enzopedroso77@gmail.com>
2025-10-07 13:51:03 -0300

    Personalizando cors para fazer deploy

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -4,13 +4,16 @@ import 'dotenv/config';
 import userRoutes from './Routes/router.js';
 
 const app = express();
-app.use(cors());
-app.use(express.json());
 
-// Prefixo para as rotas de usuário
+// Configurar CORS
+app.use(cors({
+  origin: 'http://localhost:5173', // URL do seu front-end
+  credentials: true,               // se precisar enviar cookies
+}));
+
+app.use(express.json());
 app.use('/api', userRoutes);
 
-// Rota health check
 app.get('/api/', (req, res) => {
   res.json({ status: 'OK', message: 'Servidor rodando' });
 });
@@ -19,7 +22,5 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
-
-
 
 export default app;
