45e7f8b92650d172425ffd2bb44be2e92db208a3
PedrosoEnzo <enzopedroso77@gmail.com>
2025-10-09 13:44:33 -0300

    Adicionando localhost:5173 ao server.js

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -2,10 +2,17 @@ import express from "express";
 import cors from "cors";
 import userRoutes from "./Routes/router.js";
 
+
+// ... restante do código
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+// Configure CORS
+app.use(cors({
+  origin: 'http://localhost:5173', // URL do seu frontend
+  credentials: true, // Se estiver usando cookies ou autenticação
+}));
+
 app.use(express.json());
 
 // Rotas
